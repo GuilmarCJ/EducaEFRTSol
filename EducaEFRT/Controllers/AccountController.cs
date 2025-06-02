4aacce73a446bd67a04a93c608f08e61c50feb2b
GuilmarCJ <cancan22joaquin@gmail.com>
2025-06-02 18:29:24 -0500

    Agregar archivos de proyecto.

---
+++ EducaEFRT/Controllers/AccountController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EducaEFRT.Models.DB.Repositories;
+using EducaEFRT.Models;
+using System.Web.Security;
+
+namespace EducaEFRT.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            try
+            {
+                using (var repo = new UsuarioRepository())
+                {
+                    var usuario = repo.ValidarUsuario(model.Username, model.Password);
+
+                    if (usuario != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(model.Username, model.RememberMe);
+                        return RedirectToLocal(returnUrl);
+                    }
+                }
+
+                ModelState.AddModelError("", "Nombre de usuario o contraseña incorrectos.");
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "Ocurrió un error al intentar iniciar sesión. Por favor intente nuevamente.");
+                // Loggear el error ex aquí
+            }
+
+            return View(model);
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ EducaEFRT/Models/DB/EduControlDB.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace EducaEFRT.Models.DB
+{
+    public class EduControlDB : DbContext
+    {
+        public EduControlDB() : base("name=EduControlDB")
+        {
+            // Configuración adicional para evitar problemas
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Elimina esta línea si estás usando atributos en la clase
+            // modelBuilder.Entity<Usuario>().ToTable("Usuario");
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

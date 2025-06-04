5d55e1d82056d40d6d921b7c6a3bb97c49f1c4ae
fferrn <144500785+fferrn@users.noreply.github.com>
2025-06-03 23:07:59 -0500

    :3

---
+++ EducaEFRT/Models/AsignacionCurso.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using EducaEFRT.Models;
+
+[Table("AsignacionCurso")]
+public class AsignacionCurso
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id_asignacion")]
+    public int IdAsignacion { get; set; }
+
+    [Required]
+    [Column("id_docente")]
+    public int IdDocente { get; set; }
+
+    [Required]
+    [Column("id_curso")]
+    public int IdCurso { get; set; }
+
+    [Required]
+    [Column("id_seccion")]
+    public int IdSeccion { get; set; }
+
+    [Required]
+    [Column("id_turno")]
+    public int IdTurno { get; set; }
+}
---
+++ EducaEFRT/Models/Curso.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EducaEFRT.Models
+{
+    [Table("Curso")]
+    public class Curso
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id_curso")]
+        public int IdCurso { get; set; }
+
+        [Required]
+        [Column("nombre_curso")]
+        public string NombreCurso { get; set; }
+    }
+}
